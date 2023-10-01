08414d1f54f6f011e2e9deb85aea3cf46e09c27e
Vermeulen, Z, Mr [24700746@sun.ac.za] <24700746@sun.ac.za>
2023-10-01 18:46:29 +0200

    Most basic version working

--- Assignment5.c
+++ Assignment5.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#define randomreal0() (rand()/(RAND_MAX+1.0))
 
 int L = 10;
 int LS = 100;
@@ -19,13 +20,17 @@ int main(int argc, char** argv){
     fprintf(output, "Sweep, M, |M|\n");
     int latticeSize = L*L;
     signed char* lattice = malloc(sizeof(char)*latticeSize);
-    memset(lattice, 1, sizeof(char)*latticeSize);
+    memset(lattice, -1, sizeof(char)*latticeSize);
     double J = strtod(argv[1], NULL);
     double h = strtod(argv[2], NULL);
     for(int i = -4; i <= 4; i ++){
-        deltaHsPlus[i+4] = exp(-2*(i*J+2*h));
-        //printf("%d, %f, %f\n", i, -2.0*(i*J+2*h), exp(-2*(i*J+2*h)));
-        deltaHsPlus[i+4] = exp(-2*(i*J-2*h));
+        double beta = 1;
+        deltaHsPlus[i+4] = beta*exp(-2*(i*J+2*h));
+        printf("%d, %f, %f\n", i, -2.0*(i*J+2*h), beta*exp(-2*(i*J+2*h)));
+        deltaHsMinus[i+4] = beta*exp(-2*(i*J-2*h));
+    }
+    for(int i = 0; i < 9; i ++){
+        printf("%d, %f\n", i, deltaHsMinus[i]);
     }
     int passes = 1000;
     signed char si;
@@ -34,6 +39,7 @@ int main(int argc, char** argv){
     int M;
     for(int i = 0; i < passes; i ++){
         M = 0;
+        printf("Pass %d\n", i);
         for(int j = 0; j < latticeSize; j ++){
             si = lattice[j];
             sigmaSj = 0;
@@ -44,15 +50,19 @@ int main(int argc, char** argv){
             sigmaSj += lattice[up(j)];
             sigmaSj += lattice[down(j)];
             if(si == 1){
-                //printf("%d\n", si*sigmaSj+4);
+                printf("%d\n", si*sigmaSj+4);
                 p = deltaHsPlus[si*sigmaSj+4];
-                //printf("%f\n", p);
-                if(random() < p*RAND_MAX){
+                printf("%f\n", p);
+                if(randomreal0() < p || p > 1){
                     lattice[j] = -1;
                 }
             }else{
+                //read backwards?
+                //printf("%d\n", si);
+                printf("%d\n", si*sigmaSj+4);
                 p = deltaHsMinus[si*sigmaSj+4];
-                if(random() < p*RAND_MAX){
+                printf("%f\n", p);
+                if(randomreal0() < p*RAND_MAX || p > 1){
                     lattice[j] = 1;
                 }
             }
