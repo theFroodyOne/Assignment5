521c28f036c7b313b331636903d076f667033419
Vermeulen, Z, Mr [24700746@sun.ac.za] <24700746@sun.ac.za>
2023-10-07 11:18:07 +0200

    Part 2 kinda works now

--- Assignment5.c
+++ Assignment5.c
@@ -16,60 +16,62 @@ double deltaHsMinus[9];
 #define down(j) (j >= LS-L) ? (j + L - LS): (j + L)
 
 int main(int argc, char** argv){
-    double J = strtod(argv[1], NULL);
-    double h = strtod(argv[2], NULL);
-    char* filename = malloc(sizeof(char)*101);
-    sprintf(filename,"part1data/J %.3f h %.3f L %d.csv", J, h, L);
-    FILE *output = fopen(filename, "w");
-    fprintf(output, "Sweep, M/L^2, |M|/L^2\n");
-    int latticeSize = L*L;
-    signed char* lattice = malloc(sizeof(char)*latticeSize);
-    memset(lattice, -1, sizeof(char)*latticeSize);
-    for(int i = -4; i <= 4; i ++){
-        double beta = 1;
-        deltaHsPlus[i+4] = beta*exp(-2*(i*J+2*h));
-        deltaHsMinus[i+4] = beta*exp(-2*(i*J-2*h));
-    }
-    int passes = 1000;
-    signed char si;
-    int sigmaSj;
-    double p;
-    int M;
-    double sumM = 0;
-    double sumAbsM = 0;
-    double sumM2 = 0;
-    for(int i = 0; i < passes; i ++){
-        M = 0;
-        //printf("Pass %d\n", i);
-        for(int j = 0; j < latticeSize; j ++){
-            si = lattice[j];
-            sigmaSj = 0;
-            //check neighbours
-            sigmaSj += lattice[left(j)];
-            sigmaSj += lattice[right(j)];
-            sigmaSj += lattice[up(j)];
-            sigmaSj += lattice[down(j)];
-            if(si == 1){
-                p = deltaHsPlus[si*sigmaSj+4];
-                if(randomreal0() < p || p > 1){
-                    lattice[j] = -1;
-                }
-            }else{
-                p = deltaHsMinus[si*sigmaSj+4];
-                if(randomreal0() < p*RAND_MAX || p > 1){
-                    lattice[j] = 1;
+    double h = 0;
+    FILE *output = fopen("part2data/data.csv", "w");
+    fprintf(output, "J, M/L^2, |M|/L^2\n");
+    for(int k = 0; k < 100; k ++) {
+        printf("%d\n", k);
+        double J = k/100.0;
+        int latticeSize = L * L;
+        signed char *lattice = malloc(sizeof(char) * latticeSize);
+        memset(lattice, -1, sizeof(char) * latticeSize);
+        for (int i = -4; i <= 4; i++) {
+            double beta = 1;
+            deltaHsPlus[i + 4] = beta * exp(-2 * (i * J + 2 * h));
+            deltaHsMinus[i + 4] = beta * exp(-2 * (i * J - 2 * h));
+        }
+        int passes = 10000;
+        int equilibration = passes/10;
+        signed char si;
+        int sigmaSj;
+        double p;
+        int M;
+        double sumM = 0;
+        double sumAbsM = 0;
+        for (int i = 0; i < passes; i++) {
+            M = 0;
+            //printf("Pass %d\n", i);
+            for (int j = 0; j < latticeSize; j++) {
+                si = lattice[j];
+                sigmaSj = 0;
+                //check neighbours
+                sigmaSj += lattice[left(j)];
+                sigmaSj += lattice[right(j)];
+                sigmaSj += lattice[up(j)];
+                sigmaSj += lattice[down(j)];
+                if (si == 1) {
+                    p = deltaHsPlus[si * sigmaSj + 4];
+                    if (randomreal0() < p || p > 1) {
+                        lattice[j] = -1;
+                    }
+                } else {
+                    p = deltaHsPlus[si * sigmaSj + 4];
+                    if (randomreal0() < p || p > 1) {
+                        lattice[j] = 1;
+                    }
                 }
             }
+            for (int j = 0; j < latticeSize; j++) {
+                M += lattice[j];
+            }
+            if (i >= equilibration) {
+                sumM += M;
+                sumAbsM += abs(M);
+            }
+            //todo energy stuff
         }
-        for(int j = 0; j < latticeSize; j ++){
-            M += lattice[j];
-        }
-        fprintf(output, "%d, %f, %f\n", i, (double)M/latticeSize, (double)abs(M)/latticeSize);
-
-        //todo energy stuff
+        sumM /= L * L;
+        sumAbsM /= L * L;
+        fprintf(output, "%f, %f, %f\n", J, sumM / (passes-equilibration), sumAbsM / (passes-equilibration));
     }
-    sumM /= L*L;
-    sumAbsM /= L*L;
-    sumM2 /= L*L;
-    printf("<m>: %f, <|m|>: %f, <m^2>: %f\n", sumM/passes, sumAbsM/passes, sumM2/passes);
 }
--- run.sh
+++ run.sh
@@ -1,5 +1,2 @@
 gcc -o Assignment5.out Assignment5.c -lm
-for i in 0.2 0.3 0.4 0.5 0.6
-do
-  ./Assignment5.out $i 0
-done
+./Assignment5.out
